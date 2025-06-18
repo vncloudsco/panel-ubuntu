6952fc7c05e7467694a581d5d27bdd56c39fe97b
Mạnh Tường Solo <contact@manhtuong.net>
2025-06-18 13:35:53 +0700

    Enhance rebuild_nginx.sh: streamline package installation, improve error handling, and update module download process

--- rebuild_nginx.sh
+++ rebuild_nginx.sh
@@ -9,73 +9,117 @@
 #              Please do not remove copyright. Thank!                #
 #  Please do not copy under any circumstance for commercial reason!  #
 ######################################################################
+# Exit on any error
+set -e
 
-apt-get install build-essential zlib1g-dev libpcre3 libpcre3-dev unzip uuid-dev -y
+# Install required packages
+apt-get update && apt-get install -y \
+    build-essential \
+    zlib1g-dev \
+    libpcre3 \
+    libpcre3-dev \
+    unzip \
+    uuid-dev \
+    git \
+    wget \
+    curl
 
-UPDATE_LINK="https://tinyactive.github.io/panel/update"
-MODULE_PATH="/usr/share/nginx_module"
-MODULES_LINK="https://tinyactive.github.io/panel/modules"
+# Configuration
+readonly UPDATE_LINK="https://tinyactive.github.io/panel/update"
+readonly MODULE_PATH="/usr/share/nginx_module"
+readonly MODULES_LINK="https://tinyactive.github.io/panel/modules"
+
+# Create module directory
 mkdir -p "${MODULE_PATH}"
-NGINX_VERSION=$(curl -s ${UPDATE_LINK}/version | grep "nginx_version=" | cut -f2 -d'=')
-NPS_VERSION=$(curl -s ${UPDATE_LINK}/version | grep "nps_version=" | cut -f2 -d'=')
-ngx_cache_purge_version=$(curl -s ${UPDATE_LINK}/version | grep "ngx_cache_purge_version=" | cut -f2 -d'=')
-more_clear_headers_v=$(curl -s ${UPDATE_LINK}/version | grep "more_clear_headers_v=" | cut -f2 -d'=')
-openssl_version=$(curl -s ${UPDATE_LINK}/version | grep "openssl_version=" | cut -f2 -d'=')
-pcre_version=$(curl -s ${UPDATE_LINK}/version | grep "pcre_version=" | cut -f2 -d'=')
-zlib_version=$(curl -s ${UPDATE_LINK}/version | grep "zlib_version=" | cut -f2 -d'=')
 
-cd "${MODULE_PATH}" || exit
+# Fetch version information
+echo "Fetching version information..."
+version_info=$(curl -s "${UPDATE_LINK}/version")
+NGINX_VERSION=$(echo "$version_info" | grep "nginx_version=" | cut -f2 -d'=')
+NPS_VERSION=$(echo "$version_info" | grep "nps_version=" | cut -f2 -d'=')
+ngx_cache_purge_version=$(echo "$version_info" | grep "ngx_cache_purge_version=" | cut -f2 -d'=')
+more_clear_headers_v=$(echo "$version_info" | grep "more_clear_headers_v=" | cut -f2 -d'=')
+openssl_version=$(echo "$version_info" | grep "openssl_version=" | cut -f2 -d'=')
+pcre_version=$(echo "$version_info" | grep "pcre_version=" | cut -f2 -d'=')
+zlib_version=$(echo "$version_info" | grep "zlib_version=" | cut -f2 -d'=')
 
-rm -rf "${MODULE_PATH:?}"/*
+# Validate required variables
+for var in NGINX_VERSION NPS_VERSION ngx_cache_purge_version more_clear_headers_v openssl_version pcre_version zlib_version; do
+    if [[ -z "${!var}" ]]; then
+        echo "Error: $var is empty" >&2
+        exit 1
+    fi
+done
 
-wget -O- https://github.com/apache/incubator-pagespeed-ngx/archive/v"${NPS_VERSION}".tar.gz | tar -xz
-nps_dir=$(find . -name "*pagespeed-ngx-${NPS_VERSION}" -type d)
-cd "$nps_dir" || exit
+echo "Using NGINX version: ${NGINX_VERSION}"
+
+# Clean and setup module directory
+cd "${MODULE_PATH}"
+rm -rf ./*
+
+# Download and extract modules
+echo "Downloading modules..."
+{
+    wget -q -O- "https://github.com/apache/incubator-pagespeed-ngx/archive/v${NPS_VERSION}.tar.gz" | tar -xz
+    wget -q -O- "http://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz" | tar -xz
+    wget -q -O- "${MODULES_LINK}/ngx_cache_purge-${ngx_cache_purge_version}.tar.gz" | tar -xz
+    wget -q -O- "${MODULES_LINK}/openssl-OpenSSL_${openssl_version}.tar.gz" | tar -xz
+    wget -q -O- "ftp://ftp.pcre.org/pub/pcre/pcre-${pcre_version}.tar.gz" | tar -xz
+    wget -q -O- "https://www.zlib.net/zlib-${zlib_version}.tar.gz" | tar -xz
+    wget -q -O- "${MODULES_LINK}/headers-more-nginx-module-${more_clear_headers_v}.tar.gz" | tar -xz
+} &
+
+# Setup PageSpeed
+nps_dir="incubator-pagespeed-ngx-${NPS_VERSION}"
 NPS_RELEASE_NUMBER=${NPS_VERSION/beta/}
-NPS_RELEASE_NUMBER=${NPS_VERSION/stable/}
+NPS_RELEASE_NUMBER=${NPS_RELEASE_NUMBER/stable/}
 
-psol_url=https://dl.google.com/dl/page-speed/psol/${NPS_RELEASE_NUMBER}.tar.gz
-[ -e scripts/format_binary_url.sh ] && psol_url=$(scripts/format_binary_url.sh PSOL_BINARY_URL)
-wget -O- "${psol_url}" | tar -xz
+# Wait for downloads to complete
+wait
 
-cd "${MODULE_PATH}" || exit
-wget -O- http://nginx.org/download/nginx-"${NGINX_VERSION}".tar.gz | tar -xz
-wget -O- "${MODULES_LINK}"/ngx_cache_purge-"${ngx_cache_purge_version}".tar.gz | tar -xz
-wget -O- "${MODULES_LINK}"/openssl-OpenSSL_"${openssl_version}".tar.gz | tar -xz
-wget -O- ftp://ftp.pcre.org/pub/pcre/pcre-"${pcre_version}".tar.gz | tar -xz
-wget -O- https://www.zlib.net/zlib-"${zlib_version}".tar.gz | tar -xz
-wget -O- "${MODULES_LINK}"/headers-more-nginx-module-"${more_clear_headers_v}".tar.gz | tar -xz
+cd "${nps_dir}"
+psol_url="https://dl.google.com/dl/page-speed/psol/${NPS_RELEASE_NUMBER}.tar.gz"
+[[ -e scripts/format_binary_url.sh ]] && psol_url=$(scripts/format_binary_url.sh PSOL_BINARY_URL)
+wget -q -O- "${psol_url}" | tar -xz
 
+cd "${MODULE_PATH}"
+
+# Clone brotli module
 git clone --depth 1 https://github.com/google/ngx_brotli
-cd ngx_brotli && git submodule update --init
+cd ngx_brotli && git submodule update --init --depth 1
 
-cd "${MODULE_PATH}"/nginx-"${NGINX_VERSION}"/ || exit
+# Configure and compile NGINX
+echo "Configuring NGINX..."
+cd "${MODULE_PATH}/nginx-${NGINX_VERSION}/"
 ./configure \
-    "--user=nginx" \
-    "--group=nginx" \
-    "--prefix=/usr" \
-    "--sbin-path=/usr/sbin" \
-    "--conf-path=/etc/nginx/nginx.conf" \
-    "--pid-path=/var/run/nginx.pid" \
-    "--http-log-path=/var/log/nginx/access_log" \
-    "--error-log-path=/var/log/nginx/error_log" \
-    "--without-mail_imap_module" \
-    "--without-mail_smtp_module" \
-    "--with-http_ssl_module" \
-    "--with-http_realip_module" \
-    "--with-http_stub_status_module" \
-    "--with-http_gzip_static_module" \
-    "--with-http_dav_module" \
-    "--with-http_v2_module" \
-    "--with-pcre=../pcre-${pcre_version}" \
-    "--with-pcre-jit" \
-    "--with-zlib=../zlib-${zlib_version}" \
-    "--with-openssl=../openssl-OpenSSL_${openssl_version}" \
-    "--with-openssl-opt=no-nextprotoneg" \
-    "--add-module=../ngx_cache_purge-${ngx_cache_purge_version}" \
-    "--add-module=../incubator-pagespeed-ngx-${NPS_VERSION}" \
-    "--add-module=../headers-more-nginx-module-${more_clear_headers_v}" \
-    "--add-module=../ngx_brotli" \
-    "--with-cc-opt='-D FD_SETSIZE=32768'"
+    --user=nginx \
+    --group=nginx \
+    --prefix=/usr \
+    --sbin-path=/usr/sbin \
+    --conf-path=/etc/nginx/nginx.conf \
+    --pid-path=/var/run/nginx.pid \
+    --http-log-path=/var/log/nginx/access_log \
+    --error-log-path=/var/log/nginx/error_log \
+    --without-mail_imap_module \
+    --without-mail_smtp_module \
+    --with-http_ssl_module \
+    --with-http_realip_module \
+    --with-http_stub_status_module \
+    --with-http_gzip_static_module \
+    --with-http_dav_module \
+    --with-http_v2_module \
+    --with-pcre="../pcre-${pcre_version}" \
+    --with-pcre-jit \
+    --with-zlib="../zlib-${zlib_version}" \
+    --with-openssl="../openssl-OpenSSL_${openssl_version}" \
+    --with-openssl-opt=no-nextprotoneg \
+    --add-module="../ngx_cache_purge-${ngx_cache_purge_version}" \
+    --add-module="../incubator-pagespeed-ngx-${NPS_VERSION}" \
+    --add-module="../headers-more-nginx-module-${more_clear_headers_v}" \
+    --add-module=../ngx_brotli \
+    --with-cc-opt='-D FD_SETSIZE=32768'
 
-make && make install
+echo "Compiling NGINX..."
+make -j"$(nproc)" && make install
+
+echo "NGINX rebuild completed successfully!"
